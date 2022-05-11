539edb371479b44c4315ba990f1ebbc0bc50219c
lshcherbinin <shcherbinin.l@nictt.ru>
2022-05-11 20:11:07 +0300

    Initial commit

---
+++ src/main/java/ru/leo/book_barber/BookBarberApplication.java
@@ -0,0 +1,13 @@
+package ru.leo.book_barber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBarberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookBarberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/leo/book_barber/BookBarberApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.leo.book_barber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookBarberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
